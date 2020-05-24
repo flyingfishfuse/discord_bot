51936606631f81ea2e95093d96add80ea708eb98
flyingfishfuse <46322912+flyingfishfuse@users.noreply.github.com>
2020-05-24 09:29:03 -0500

    Update properties_lookup.py

--- properties_lookup.py
+++ properties_lookup.py
@@ -244,33 +244,33 @@ class Element_lookup(commands.Cog):
                         #capitalize if string and return value, feed to lookup function,, feed
                         # return value to reply function
                         Element_lookup.get_basic_properties(element_id_user_input)
-                        await Element_lookup.reply_to_query(lookup_output_container)
+                        Element_lookup.reply_to_query(lookup_output_container)
                         #send the message as a STRING, we kept it a LIST all the way to here
                         #await ctx.send(list_to_string(lookup_output_container))
                     # so now you got the basic structure of the control loop!
                     elif specifics_requested.lower()  == "physical":
                         Element_lookup.get_physical_properties(element_id_user_input)
-                        await Element_lookup.reply_to_query(lookup_output_container)
+                        Element_lookup.reply_to_query(lookup_output_container)
                         #await ctx.send(list_to_string(lookup_output_container))
                     elif specifics_requested.lower()  == "chemical":
                         Element_lookup.get_chemical_properties(element_id_user_input)
-                        await Element_lookup.reply_to_query(lookup_output_container)
+                        Element_lookup.reply_to_query(lookup_output_container)
                         #await ctx.send(list_to_string(lookup_output_container))
                     elif specifics_requested.lower()  == "nuclear":
                         Element_lookup.get_nuclear_properties(element_id_user_input)
-                        await Element_lookup.reply_to_query(lookup_output_container)
+                        Element_lookup.reply_to_query(lookup_output_container)
                         #await ctx.send(list_to_string(lookup_output_container))
                     elif specifics_requested.lower()  == "ionization":
                         Element_lookup.get_ionization_energy(element_id_user_input)
-                        await Element_lookup.reply_to_query(lookup_output_container)
+                        Element_lookup.reply_to_query(lookup_output_container)
                         #await ctx.send(list_to_string(lookup_output_container))
                     elif specifics_requested.lower()  == "isotopes":
                         Element_lookup.get_isotopes(element_id_user_input)
-                        await Element_lookup.reply_to_query(lookup_output_container)
+                        Element_lookup.reply_to_query(lookup_output_container)
                         #await ctx.send(list_to_string(lookup_output_container))
                     elif specifics_requested.lower()  == "oxistates":
-                        await Element_lookup.get_oxistates(element_id_user_input)
-                        await Element_lookup.reply_to_query(lookup_output_container)
+                        Element_lookup.get_oxistates(element_id_user_input)
+                        Element_lookup.reply_to_query(lookup_output_container)
                         #await ctx.send(list_to_string(lookup_output_container))
                 # input given by user was NOT found in the validation data
                 else:
