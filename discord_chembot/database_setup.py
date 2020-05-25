6c0ed6cdb5e3e3f57b2ff55a23b10572276ba359
flyingfishfuse <46322912+flyingfishfuse@users.noreply.github.com>
2020-05-25 05:47:41 -0500

    Update database_setup.py

--- discord_chembot/database_setup.py
+++ discord_chembot/database_setup.py
@@ -4,6 +4,7 @@ from flask import Flask, render_template, Response, Request ,Config
 from flask_sqlalchemy import SQLAlchemy
 
 # took this from my game, gonna change it up
+# needed a template
 DATABASE_HOST      = "localhost"
 DATABASE           = "solar_empire-python"
 DATABASE_USER      = "moop"
@@ -26,3 +27,50 @@ database = SQLAlchemy(solar_empire_server)
 database.init_app(solar_empire_server)
 #One to many relationship
 #parent
+
+class User(database.Model):
+    __tablename__ = 'User'
+    #PARENT: of UserShip, Primary key must link (be the same)
+    id            = database.Column(database.Integer, primary_key = True, unique=True, autoincrement=True)
+    #reference to CHILD then reference to SELF
+    userid       = database.Column(database.relationship('UserShip', \
+                            primaryjoin = 'and_(User.id == UserShip.user_id)' , \
+                            backref     = 'User' , \
+                            uselist     = False ))
+#    userid        = database.Column(database.Integer)
+    username      = database.Column(database.String(64), index=True)
+    email         = database.Column(database.String(120), index=True)
+    password_hash = database.Column(database.String(128))
+
+    def __repr__(self):
+        return '<User id:{} name: {} >'.format(self.user_id , self.username)
+
+class UserShip(User):
+    __tablename__  = 'UserShip'
+    #CHILD OF USER: the primary key must link
+    #ID is a "universal" identification for the db, SHIP_ID will be a variable 
+    # used locally in the system that that user and ship are interacting in
+    #id             = database.Column(database.Integer, primary_key=True)
+    # The PRIMARY key that is linked bewteen the two, with a new name, the field you want to inherit, is declared next.
+    #user_id        = database.Column(database.Integer, database.ForeignKey('User.userid'), nullable=False)
+    ship_id        = database.Column(database.Integer)
+    ship_name      = database.Column(database.String(128))
+    # list of 1-whatever of ships
+    ship_type      = database.Column(database.Integer)
+    # PARENT OF GENERIC SHIP: the clsses must link like this, backref is __tablename__
+    #linked_user    = database.relationship('GenericShip',backref='usership',uselist=False)
+    def __repr__(self):
+        return '<User id:{} name: {} >'.format(self.ship_id , self.ship_name)
+
+
+admin = User(username='admin', userid = 1,  email="test@wat" , password_hash = 'passwordadmin')
+usership = UserShip(ship_id=3, ship_name='user ship', ship_type=2)
+database.create_all()
+database.session.add(admin)
+database.session.add(usership)
+
+database.session.commit()
+#solar_empire_server.run()
+
+def user_by_id(id_of_user):
+    return User.query.all.filter_by(user_id = id_of_user).first()
