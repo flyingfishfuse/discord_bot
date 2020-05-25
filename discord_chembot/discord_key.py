f68dc784f28cc7cb17dedc7ce06eb47f0c2c045b
flyingfishfuse <46322912+flyingfishfuse@users.noreply.github.com>
2020-05-25 05:17:23 -0500

    Create discord_key.py

---
+++ discord_chembot/discord_key.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+import os
+discord_bot_token   = "IM A DOOFUS FACE"
+COMMAND_PREFIX      = "."
+BOT_PERMISSIONS     = 92160
+devs                = [712737412018733076]
+cog_directory_files = os.listdir("./cogs")
+load_cogs           = False
