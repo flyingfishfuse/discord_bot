f2071a001c3591953ec711426f3cbd6c6afee442
flyingfishfuse <46322912+flyingfishfuse@users.noreply.github.com>
2020-05-25 05:22:28 -0500

    Delete discord_key.py

--- discord_key.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python3
-discord_bot_token ="NzEyNzM3NDEyMDE4NzMzMDc2.XsY1zg.r3bEtL01JHCDuIfnRZVm-Mg7OuA"
-COMMAND_PREFIX    = "$SLOTMACHINE"
-BOT_PERMISSIONS   = 92160
-devs = [712737412018733076]
-cog_directory_files = os.listdir("./cogs")
-load_cogs = False
