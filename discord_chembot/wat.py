276e4a70c12e71e1983370c545413f988e507148
flyingfishfuse <46322912+flyingfishfuse@users.noreply.github.com>
2020-08-01 09:27:46 -0500

    Add files via upload

--- discord_chembot/wat.py
+++ discord_chembot/wat.py
@@ -1,9 +1,45 @@
 import lxml
 import requests
 from bs4 import BeautifulSoup
-from variables_for_reality import TESTING
-from variables_for_reality import API_BASE_URL, pubchem_search_types
-from variables_for_reality import greenprint, redprint, blueprint
+from requests.utils import requote_uri
+
+# Filter None values from kwargs
+#kwargs = dict((k, v) for k, v in kwargs.items() if v is not None)
+
+#################################################################################
+# Color Printing 
+#################################################################################
+import colorama
+from colorama import init
+init()
+from colorama import Fore, Back, Style
+#Fore: BLACK, RED, GREEN, YELLOW, BLUE, MAGENTA, CYAN, WHITE, RESET.
+#Back: BLACK, RED, GREEN, YELLOW, BLUE, MAGENTA, CYAN, WHITE, RESET.
+#Style: DIM, NORMAL, BRIGHT, RESET_ALL
+
+#make them global scope for testing purposes
+blueprint = lambda text: print(Fore.BLUE + ' ' +  text + ' ' + Style.RESET_ALL)
+greenprint = lambda text: print(Fore.GREEN + ' ' +  text + ' ' + Style.RESET_ALL)
+redprint = lambda text: print(Fore.RED + ' ' +  text + ' ' + Style.RESET_ALL)
+
+################################################################################
+## TESTING VARS
+################################################################################
+
+TESTING = True
+#TESTING = False
+#The sqlite :memory: identifier is the default if no filepath is present. 
+# Specify sqlite:// and nothing else:
+#e = create_engine('sqlite://')
+TEST_DB = 'sqlite://'
+
+################################################################################
+################################################################################
+
+# pubchem REST API service
+pubchem_search_types = ["iupac_name", "cid", "cas"]
+API_BASE_URL         = 'https://pubchem.ncbi.nlm.nih.gov/rest/pug'
+
 #just the usual linter errors
 ###############################################################################
 # Additional REST request required, regrettably, return 
@@ -33,10 +69,13 @@ class pubchemREST_Description_Request():
 
     def do_the_thing(self):
         #finalized URL        
-        self.request_url        = API_BASE_URL + "compound/" + self.thing_type + "/" +\
-                                  self.record_to_request + "/description/XML"
+        self.request_url        = requote_uri("{}/compound/{}/{}/description/XML".format(\
+                                    API_BASE_URL,self.thing_type,self.record_to_request))
+        blueprint("[+] Requesting: ")
+        redprint(self.request_url)
         #make the request
         self.request_return     = requests.get(self.request_url)
+        print(self.request_return)
         #make some soup
         self.soupyresults       = BeautifulSoup(self.request_return.content , 'lxml')
         #serve up the Descriptions
@@ -47,3 +86,16 @@ class pubchemREST_Description_Request():
         else:
             return "No Description Available in XML REST response"
             #lambda tag:  tag.name =='Description' and tag.has_key('class') and tag['class'] == self.container_name)
+
+if TESTING == True:
+    #pubchemREST_Description_Request("methanol","iupac_name")
+    #pubchemREST_Description_Request("caffeine","iupac_name")
+    #pubchemREST_Description_Request("water","iupac_name")
+    pubchemREST_Description_Request("methylene chloride","iupac_name")
+    #ozone
+    #pubchemREST_Description_Request("24823","cid")
+    # phosphorus
+    #pubchemREST_Description_Request("5462309","cid")
+    # bisphenol-a
+    pubchemREST_Description_Request("6623","cid")
+
