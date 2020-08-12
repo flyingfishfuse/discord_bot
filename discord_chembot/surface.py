907a5b8eefea3144707afcc096a7834ed58399a2
flyingfishfuse <46322912+flyingfishfuse@users.noreply.github.com>
2020-08-12 05:10:58 -0500

    Add files via upload

--- discord_chembot/surface.py
+++ discord_chembot/surface.py
@@ -16,7 +16,7 @@ from database_setup import Database_functions as database
 
 #should it inherit from Compound?
 class Surface():
-    def __init__(self,  material : str , doping: list , x_limit : int, y_limit : int, doping_coefficient: list):
+    def __init__(self,  material , doping_material , x_limit : int, y_limit : int, z_limit : int, doping_coefficient: list):
         '''
         Material is a single Compound or dict
 
@@ -33,32 +33,37 @@ class Surface():
         '''
         self.x_dimension        = x_limit
         self.y_dimension        = y_limit
-        self.substrate          = material.formula
-        self.doping             = doping
+        self.z_dimension        = z_limit
+        self.substrate          = material
+        self.doping_material    = doping_material
         self.doping_coeffecient = doping_coefficient
+        self.make_grid(self.substrate, self.doping_material)
 
+    def make_grid(self, substrate, doping_material : list):
+        '''
 
-    def make_grid(substrate: str, doping_material, depth: int):
         '''
-        substrate is an Atom
-        material is an Atom from ase
-        depth is layers
-        '''
-        plane1 = [Atom(self.substrate.formula)]
+        self.substrate = substrate
+        if ASE == True:
+            plane1 = [Atom(self.substrate.formula)]
+            for each in doping_material:
+                plane1.append(Atom(each))
+        elif SUBSTANCE == True:
+            # do a db call here to grab entities
+            plane1 = [Compound(self.substrate.formula)]
+            for each in doping_material:
+                plane1.append(Atom(each))
+        else:
+            print("wat")
         
-        for each in doping_material:
-            plane1.append(Atom(each))
-        doping_coefficient = [0.998 , 0.002]
+        derp = numpy.random.choice(a    = plane1                     ,\
+                                   size = (self.x_dimension , self.y_dimension, self.z_dimension),\
+                                   p    = self.doping_coeffecient)
         
-        derp = numpy.random.choice(a    = herp                     ,\
-                                   size = (self.x_dimension , self.y_dimension, self.thickness),\
-                                   p    = doping_coeffecient)
-        
-        thing_to_place = Atom(symbol=doping_material)
         #empty array to init
-        self.doped_substrate = []
+        self.doped_substrate = derp
         
-
+Surface("Pd", "Co", 100, 100, 10, [0.998 , 0.002])
 #x_lim = 100
 #y_lim = 100
 #herp = [Atom("Pd"), Atom("Co")]
